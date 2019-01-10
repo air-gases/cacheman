49976e4f46a9a7017a7240aebd72cc9190dc3491
Aofei Sheng <aofei@aofeisheng.com>
2019-01-10 12:15:30 +0800

    feat: add implementation

---
+++ cacheman.go
@@ -0,0 +1,76 @@
+package cacheman
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/aofei/air"
+)
+
+// GasConfig is a set of configurations for the `Gas()`.
+type GasConfig struct {
+	MustRevalidate  bool
+	NoCache         bool
+	NoStore         bool
+	NoTransform     bool
+	Public          bool
+	Private         bool
+	ProxyRevalidate bool
+	MaxAge          int
+	SMaxAge         int
+}
+
+// Gas returns an `air.Gas` that is used to manage "Cache-Control" header based
+// on the gc.
+func Gas(gc GasConfig) air.Gas {
+	ds := []string{}
+	if gc.MustRevalidate {
+		ds = append(ds, "must-revalidate")
+	}
+
+	if gc.NoCache {
+		ds = append(ds, "no-cache")
+	}
+
+	if gc.NoStore {
+		ds = append(ds, "no-store")
+	}
+
+	if gc.NoTransform {
+		ds = append(ds, "no-transform")
+	}
+
+	if gc.Public {
+		ds = append(ds, "public")
+	}
+
+	if gc.Private {
+		ds = append(ds, "private")
+	}
+
+	if gc.ProxyRevalidate {
+		ds = append(ds, "proxy-revalidate")
+	}
+
+	if gc.MaxAge >= 0 {
+		ds = append(ds, fmt.Sprintf("max-age=%d", gc.MaxAge))
+	}
+
+	if gc.SMaxAge >= 0 {
+		ds = append(ds, fmt.Sprintf("s-maxage=%d", gc.SMaxAge))
+	}
+
+	directives := strings.Join(ds, ", ")
+
+	return func(next air.Handler) air.Handler {
+		return func(req *air.Request, res *air.Response) error {
+			res.Header.Set("Cache-Control", directives)
+			err := next(req, res)
+			if err != nil && !res.Written {
+				res.Header.Del("Cache-Control")
+			}
+
+			return err
+		}
+	}
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/air-gases/cacheman
+
+require github.com/aofei/air v0.6.0
